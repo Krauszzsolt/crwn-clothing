6772bf83234ad0a5d89041ff317442a18be8e261
krauszzsolt <Grenma97>
2021-12-02 14:36:45 +0100

    add firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,28 @@ import ShopPage from './page/shop/shop.component';
 import SignInAndSignUpPage from './page/sign-in-and-sing-up/sign-in-and-sing-up.component';
 
 function App() {
+  // Import the functions you need from the SDKs you need
+  // import { initializeApp } from 'firebase/app';
+  // import { getAnalytics } from 'firebase/analytics';
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDdqiaMHiX8hPH_DpUFVf0ozvP3BgXApss',
+    authDomain: 'crwn-db-42a2b.firebaseapp.com',
+    projectId: 'crwn-db-42a2b',
+    storageBucket: 'crwn-db-42a2b.appspot.com',
+    messagingSenderId: '95285597966',
+    appId: '1:95285597966:web:5ee390af7c6ad4ed7e295f',
+    measurementId: 'G-8VSBSP97N0',
+  };
+
+  // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+  // const analytics = getAnalytics(app);
+
   return (
     <div className='App'>
       <Header />
